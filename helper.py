5051f368a5a85429c060421649e9ad69404e12f5
Bob-Z <carabobz@gmail.com>
2023-11-06 14:54:03 +0100

    Fix coordinate calculation

--- helper.py
+++ helper.py
@@ -14,24 +14,30 @@ def lat_lon_to_distance(lat1, lat2, lon1, lon2):
 
 # Return X from latitude
 def lat_to_x(lat):
-    return lat_lon_to_distance(float(lat), bbox.coord["south"], bbox.coord["east"], bbox.coord["east"])
+    if float(lat) < bbox.coord["south"]:
+        return lat_lon_to_distance(float(lat), bbox.coord["south"], bbox.coord["east"], bbox.coord["east"])
+    else:
+        return -lat_lon_to_distance(float(lat), bbox.coord["south"], bbox.coord["east"], bbox.coord["east"])
 
 
 # Return Y from longitude
 def lon_to_y(lon):
-    return lat_lon_to_distance(bbox.coord["north"], bbox.coord["north"], bbox.coord["east"], float(lon))
+    if float(lon) < bbox.coord["east"]:
+        return lat_lon_to_distance(bbox.coord["north"], bbox.coord["north"], bbox.coord["east"], float(lon))
+    else:
+        return -lat_lon_to_distance(bbox.coord["north"], bbox.coord["north"], bbox.coord["east"], float(lon))
 
 
 # Get an array of 3 vertices
 # A vertex is an array of 3 values (x,y,z)
 # Return an array of 3 values
 def calc_norm(triangle_vertices):
-    ux, uy, uz = [triangle_vertices[1][0] - triangle_vertices[0][0], triangle_vertices[1][1] - triangle_vertices[0][1], triangle_vertices[1][2] - triangle_vertices[0][2]]  # first vector
-    vx, vy, vz = [triangle_vertices[2][0] - triangle_vertices[0][0], triangle_vertices[2][1] - triangle_vertices[0][1], triangle_vertices[2][2] - triangle_vertices[0][2]]  # sec vector
+    ux, uy, uz = [triangle_vertices[1][0] - triangle_vertices[0][0], triangle_vertices[1][1] - triangle_vertices[0][1],
+                  triangle_vertices[1][2] - triangle_vertices[0][2]]  # first vector
+    vx, vy, vz = [triangle_vertices[2][0] - triangle_vertices[0][0], triangle_vertices[2][1] - triangle_vertices[0][1],
+                  triangle_vertices[2][2] - triangle_vertices[0][2]]  # sec vector
 
     cross = [uy * vz - uz * vy, uz * vx - ux * vz, ux * vy - uy * vx]  # cross product
 
     # Normalize
     return cross / numpy.linalg.norm(cross)
-
-
