1eada2ef7632672c01028a592e5a7e79d4fc6f3f
Bob-Z <carabobz@gmail.com>
2024-01-02 17:08:23 +0100

    Pop height and min_height tags

--- gen_building.py
+++ gen_building.py
@@ -21,17 +21,18 @@ def process(osm_data):
 
 
 def build_from_relation(osm_data, rel):
+    height = None
+    if "height" in rel.tags:
+        height = float(rel.tags["height"])
+        rel.tags.pop("height")
+
+    min_height = 0.0
+    if "min_height" in rel.tags:
+        min_height = float(rel.tags["min_height"])
+        height = height - min_height
+        rel.tags.pop("min_height")
+
     for member in rel.members:
-        if "height" in rel.tags:
-            height = float(rel.tags["height"])
-        else:
-            height = None
-
-        min_height = 0.0
-        if "min_height" in rel.tags:
-            min_height = float(rel.tags["min_height"])
-            height = height - min_height
-
         way = osm.get_way_by_id(osm_data, member.ref)
         if way is not None:
             if member.role == "outer":
@@ -40,23 +41,22 @@ def build_from_relation(osm_data, rel):
             # elif member.role == "inner":
 
 
-def build_from_way(way, forced_height=None, min_height=0.0, from_relation=False):
+def build_from_way(way, height=None, min_height=0.0, from_relation=False):
     is_barrier = False
     if len(way.nodes) < 3:
         is_barrier = True
 
-    height = config.data["building_level_height"]
-    if forced_height is None:
-        if "building:levels" in way.tags:
-            level_qty = way.tags["building:levels"]
-            way.tags.pop("building:levels")
-            height = float(level_qty) * config.data["building_level_height"]
+    if height is None:
+        height = config.data["building_level_height"]
 
-        if "height" in way.tags:
-            height = float(way.tags["height"])
-            way.tags.pop("height")
-    else:
-        height = forced_height
+    if "building:levels" in way.tags:
+        level_qty = way.tags["building:levels"]
+        way.tags.pop("building:levels")
+        height = float(level_qty) * config.data["building_level_height"]
+
+    if "height" in way.tags:
+        height = float(way.tags["height"])
+        way.tags.pop("height")
 
     if from_relation is True:
         object_3d.create_all_object_file(way.nodes, height, z=min_height, wall_texture=config.data["wall_texture"],
