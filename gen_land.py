27be6d40c331d6206e9c6e238e22f546f78c12a0
Bob-Z <carabobz@gmail.com>
2024-01-09 22:43:03 +0100

    Tweak lands' heights

--- gen_land.py
+++ gen_land.py
@@ -8,14 +8,14 @@ def process(entity, osm_data=None):
     # z = -gvar.GROUND_LEVEL  -> to be sure that lands starts at the bottom of the sea
     if "leisure" in entity.tags:
         if entity.tags["leisure"] == "park":
-            create_from_entity(osm_data, entity, height=gvar.GROUND_LEVEL + config.data["grass_height"],
+            create_from_entity(osm_data, entity, height=gvar.GROUND_LEVEL + config.data["park_height"],
                                z=-gvar.GROUND_LEVEL,
                                wall_texture="mapgen_grass_dandelion",
                                roof_texture="mapgen_grass_dandelion")
             entity.tags.pop("leisure")
             return True
         if entity.tags["leisure"] == "pitch":
-            create_from_entity(osm_data, entity, height=gvar.GROUND_LEVEL + config.data["grass_height"],
+            create_from_entity(osm_data, entity, height=gvar.GROUND_LEVEL + config.data["park_height"],
                                z=-gvar.GROUND_LEVEL,
                                wall_texture="mapgen_grass",
                                roof_texture="mapgen_grass")
@@ -71,7 +71,7 @@ def create_from_entity(osm_data, entity, height, z,
                                                      roof_texture=roof_texture,
                                                      roof_texture_generator=roof_texture_generator)
                 elif member.role == "inner":
-                    object_3d.create_all_object_file(way.nodes, height=height, z=z,
+                    object_3d.create_all_object_file(way.nodes, height=config.data["island_height"], z=z,
                                                      wall_texture=island_wall_texture,
                                                      roof_texture=island_roof_texture,
                                                      roof_texture_generator=roof_texture_generator)
