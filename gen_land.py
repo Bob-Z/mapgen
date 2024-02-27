7d170e48e4a5512a38218bb6fdb57bf773450bc0
Bob-Z <carabobz@gmail.com>
2024-02-27 08:31:08 +0100

    More lands

--- gen_land.py
+++ gen_land.py
@@ -1,5 +1,4 @@
 import ogre_map_surface
-import ogre_map_vegetation
 import ror_tobj_file
 
 
@@ -30,24 +29,17 @@ def process(entity, osm_data=None):
                 ogre_map_surface.draw_grass_entity(osm_data, entity)
             entity.tags.pop("leisure")
             return True
-
-    if "natural" in entity.tags:
-
-        if entity.tags["natural"] == "sand" or entity.tags["natural"] == "beach":
-            ogre_map_surface.draw_sand_entity(osm_data, entity)
-            entity.tags.pop("natural")
+        if entity.tags["leisure"] == "sports_centre":
+            ogre_map_surface.draw_asphalt_entity(osm_data, entity)
+            entity.tags.pop("leisure")
             return True
-        if entity.tags["natural"] == "grassland" or entity.tags["natural"] == "scrub":
-            ogre_map_surface.draw_grass_entity(osm_data, entity)
-            entity.tags.pop("natural")
-            return True
-        if entity.tags["natural"] == "wood":
-            ror_tobj_file.add_tree(osm_data, entity, 0.80, 1.90, -10, "tree.mesh", "tree.mesh")
-            entity.tags.pop("natural")
-            return True
-        if entity.tags["natural"] == "shingle":
-            ogre_map_surface.draw_gravel_entity(osm_data, entity)
-            entity.tags.pop("natural")
+        if entity.tags["leisure"] == "track":
+            if "surface" in entity.tags and entity.tags["surface"] == "compacted":
+                ogre_map_surface.draw_sand_entity(osm_data, entity)
+                entity.tags.pop("surface")
+            else:
+                ogre_map_surface.draw_asphalt_entity(osm_data, entity)
+            entity.tags.pop("leisure")
             return True
 
     if "landuse" in entity.tags:
@@ -56,17 +48,36 @@ def process(entity, osm_data=None):
             entity.tags.pop("landuse")
             return True
         if entity.tags["landuse"] == "construction" or entity.tags["landuse"] == "industrial" or entity.tags[
-            "landuse"] == "residential" or entity.tags["landuse"] == "retail":
+            "landuse"] == "residential" or entity.tags["landuse"] == "retail" or entity.tags["landuse"] == "education":
             ogre_map_surface.draw_asphalt_entity(osm_data, entity)
             entity.tags.pop("landuse")
             return True
         if entity.tags["landuse"] == "forest":
-            ror_tobj_file.add_tree(osm_data, entity, 0.80, 1.90, -10, "tree.mesh", "tree.mesh")
+            ror_tobj_file.add_tree(osm_data, entity, 0.50, 1.40, -10, "tree.mesh", "tree.mesh")
             entity.tags.pop("landuse")
             return True
         if entity.tags["landuse"] == "orchard":
             ror_tobj_file.add_tree(osm_data, entity, 1.0, 1.0, 15, "tree2.mesh", "tree2.mesh")
             entity.tags.pop("landuse")
+            return True
+
+    if "natural" in entity.tags:
+        if entity.tags["natural"] == "sand" or entity.tags["natural"] == "beach":
+            ogre_map_surface.draw_sand_entity(osm_data, entity)
+            entity.tags.pop("natural")
+            return True
+        if entity.tags["natural"] == "grassland" or entity.tags["natural"] == "scrub":
+            ogre_map_surface.draw_grass_entity(osm_data, entity)
+            entity.tags.pop("natural")
+            return True
+        if entity.tags["natural"] == "wood" or entity.tags["natural"] == "tree_group":
+            ror_tobj_file.add_tree(osm_data, entity, 0.50, 1.40, -10, "tree.mesh", "tree.mesh")
+            entity.tags.pop("natural")
+            return True
+
+        if entity.tags["natural"] == "shingle":
+            ogre_map_surface.draw_gravel_entity(osm_data, entity)
+            entity.tags.pop("natural")
             return True
 
     if "place" in entity.tags:
@@ -77,5 +88,12 @@ def process(entity, osm_data=None):
                 entity.tags.pop("surface")
             entity.tags.pop("place")
             return True
+
+    if "surface" in entity.tags:
+        if entity.tags["surface"] == "compacted" and len(entity.tags) == 1: # only one "surface tag
+            ogre_map_surface.draw_sand_entity(osm_data, entity)
+            entity.tags.pop("surface")
+            return True
+
 
     return False
