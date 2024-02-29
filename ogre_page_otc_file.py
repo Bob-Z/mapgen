6dbbb1433ff1f5f819f8437ab576a3078d918baf
Bob-Z <carabobz@gmail.com>
2024-02-29 11:31:36 +0100

    Lighter dust

--- ogre_page_otc_file.py
+++ ogre_page_otc_file.py
@@ -7,7 +7,7 @@ def create_file():
             config.data["map_name"] + "_height.png\n" + "\
 6\n\
 ; worldSize, diffusespecular, normalheight, blendmap, blendmapmode, alpha\n\
-6, dirt_diffusespecular.dds, dirt_normalheight.dds\n \
+4, dirt_diffusespecular.dds, dirt_normalheight.dds\n \
 1, asphalt_diffusespecular.dds, asphalt_normalheight.dds, surface0.png, R, 0.9\n\
 6, rock_diffusespecular.dds, rock_normalheight.dds, surface0.png, G, 0.9\n\
 5, sand_diffusespecular.dds, sand_normalheight.dds, surface0.png, B, 0.9\n\
