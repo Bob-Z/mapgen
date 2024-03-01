1899658eebc50ebcab1f9dcf70933ebeaebd53a8
Bob-Z <carabobz@gmail.com>
2024-03-01 15:47:30 +0100

    Add some building

--- gen_building.py
+++ gen_building.py
@@ -2,8 +2,14 @@ import object_3d
 import config
 import osm
 
-build_tag_value = [["type", "building"], ['man_made', 'street_cabinet'], ['man_made', 'reservoir_covered'],
-                   ['man_made', 'pumping_station']]
+build_tag_value = [
+    ["type", "building"],
+    ['man_made', 'street_cabinet'],
+    ['man_made', 'reservoir_covered'],
+    ['man_made', 'pumping_station'],
+    ['man_made', 'wastewater_plant']
+]
+
 build_tag = ["building:part", "building"]
 negative_tag_value = [["amenity", "shelter"], ["building", "roof"]]
 if config.data["hide_wiki"] is True:
