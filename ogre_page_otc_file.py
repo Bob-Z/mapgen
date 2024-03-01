9f5c64e760c2ba16f578c37af1eed74f13d26720
Bob-Z <carabobz@gmail.com>
2024-03-01 15:51:25 +0100

    Forgot to commit this with previous commit

--- ogre_page_otc_file.py
+++ ogre_page_otc_file.py
@@ -7,9 +7,10 @@ def create_file():
             config.data["map_name"] + "_height.png\n" + "\
 6\n\
 ; worldSize, diffusespecular, normalheight, blendmap, blendmapmode, alpha\n\
-4, dirt_diffusespecular.dds, dirt_normalheight.dds\n \
+4, dirt_diffusespecular.dds, dirt_normalheight.dds\n\
 1, asphalt_diffusespecular.dds, asphalt_normalheight.dds, surface0.png, R, 0.9\n\
-6, rock_diffusespecular.dds, rock_normalheight.dds, surface0.png, G, 0.9\n\
-5, sand_diffusespecular.dds, sand_normalheight.dds, surface0.png, B, 0.9\n\
-4, grass_diffusespecular.dds, grass_normalheight.dds, surface1.png, R, 0.8\n\
-3.3, gravel_diffusespecular.dds, gravel_normalheight.dds, surface1.png, G, 0.8\n")
+6, rock_diffusespecular.dds, rock_normalheight.dds, surface1.png, R, 0.9\n\
+5, sand_diffusespecular.dds, sand_normalheight.dds, surface2.png, R, 0.9\n\
+4, grass_diffusespecular.dds, grass_normalheight.dds, surface3.png, R, 0.9\n\
+3, gravel_diffusespecular.dds, gravel_normalheight.dds, surface4.png, R, 0.9\n")
+
