12246fd041d7aa6fc92d6f60371587d15730336d
Bob-Z <carabobz@gmail.com>
2026-09-03 23:01:14 +0200

    Manage "Point" geometry

--- osm.py
+++ osm.py
@@ -271,6 +271,8 @@ def get_coord_from_feature(feature):
         all_all_coord = [feature["geometry"]["coordinates"]]
     elif feature["geometry"]["type"] == "MultiLineString":
         all_all_coord = feature["geometry"]["coordinates"]
+    elif feature["geometry"]["type"] == "Point":
+        all_all_coord = [feature["geometry"]["coordinates"]]
     else:
         print("Unsupported geometry " + feature["geometry"]["type"])
         sys.exit(-1)
